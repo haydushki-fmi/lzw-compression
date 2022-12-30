d4dae6d56987cc4499b4e6e29a67f7a80eda6d15
Stoyan Haydushki <haydushki@uni-sofia.bg>
2022-12-30 14:29:04 +0200

    Update Set interface
    Make contains() const;
    Fix search() to return a pointer not a reference;
    Add size() and at() insted of iterator stuff.

--- src/common_data_structures/Set.hpp
+++ src/common_data_structures/Set.hpp
@@ -2,20 +2,20 @@
 #define SET_HPP
 
 #include "common_data_structures/Sortable.hpp"
+
 namespace common_data_structures {
 
 /**
  * @brief Represents a Set interface.
  */
-template<class T>
-requires Sortable<T> // TODO: Use std::totally_ordered?
-    class Set
+template<class T> // TODO: Use std::totally_ordered?
+requires Sortable<T> class Set
 {
 public:
     /**
-   * @brief Insert an item into the set.
-   * @param item Item to be inserted.
-   */
+    * @brief Insert an item into the set.
+    * @param item Item to be inserted.
+    */
     virtual void insert(const T &item) = 0;
 
     /**
@@ -23,7 +23,7 @@ public:
     * @param item Item to check for.
     * @return True if item is present in the set.
     */
-    virtual bool contains(const T &item) = 0;
+    virtual bool contains(const T &item) const = 0;
 
     /**
     * @brief Remove an item from the set.
@@ -32,20 +32,40 @@ public:
     virtual void remove(const T &item) = 0;
 
     /**
-    * @brief Search for an item in the set
+    * @brief Search for an item in the set.
     * @param item Item to search for.
-    * @return Reference to the desired item. Nullptr if item doesn't exist.
+    * @return Pointer to the desired item. Nullptr if item doesn't exist.
     */
-    virtual const T &search(const T &item) const = 0;
+    virtual const T *search(const T &item) const = 0;
 
     /**
-    * @brief Search for an item in the set
+    * @brief Search for an item in the set.
     * @param item Item to search for.
-    * @return Reference to the desired item. Nullptr if item doesn't exist.
+    * @return Pointer to the desired item. Nullptr if item doesn't exist.
     */
-    virtual T &search(const T &item) = 0;
+    virtual T *search(const T &item) = 0;
 
-    // TODO: Add iterator stuff
+    /**
+     * @brief Get the number of items in the st.
+     * @return Size of the set.
+     */
+    virtual std::size_t size() const = 0;
+
+    /**
+     * @brief Get item at index.
+     * @param index Index to get item at.
+     * @return Reference to the desired item.
+     * @throws std::invalid_argument when index is invalid.
+     */
+    virtual const T &at(std::size_t index) const = 0;
+
+    /**
+     * @brief Get item at index.
+     * @param index Index to get item at.
+     * @return Reference to the desired item.
+     * @throws std::invalid_argument when index is invalid.
+     */
+    virtual T &at(std::size_t index) = 0;
 };
 } // namespace common_data_structures
 
