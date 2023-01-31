62a26878a07d53e9876ff2ce230260e9a2935355
Stoyan Haydushki <haydushki@uni-sofia.bg>
2023-01-31 14:29:45 +0200

    Remove SetFactory

--- src/common_data_structures/SetFactory.cpp
+++
@@ -1,8 +0,0 @@
-#include "SetFactory.hpp"
-
-// NOTE: Why doesn't it find this?
-//common_data_structures::SetFactory &common_data_structures::SetFactory::getSetFactory()
-//{
-//    static SetFactory theFactory;
-//    return theFactory;
-//}
---
+++ src/common_data_structures/unused.cpp
@@ -0,0 +1,1 @@
+// The sole purpose of this file is to stop CMake from complaining
